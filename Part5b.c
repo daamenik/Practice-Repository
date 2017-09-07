bbab27b8fdff8ac82c09e12ce7c3cdc26fc10d41
obstacole <31168774+obstacole@users.noreply.github.com>
2017-09-07 02:16:40 -0500

    Add files via upload

---
+++ Part5b.c
@@ -0,0 +1,16 @@
+/*
+  Created by: Cole McReynolds
+  Date: 9/6/17
+  The purpose of this program is to print a basic
+  string and to be able to send it through
+  GitHub for another person to collaborate on.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  printf("Cole McReynolds\n");
+
+  return 0;
+}
